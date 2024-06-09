58e6ec92b4a2c5041203ce0663f753741f282e22
Mohd Asri Omar <62752924+asriomar@users.noreply.github.com>
2024-06-09 12:16:38 +0800

    Add files via upload

---
+++ src/components/UserList.jsx
@@ -0,0 +1,57 @@
+// src/UserList.js
+import React, { useState, useEffect } from 'react';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        setUsers(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <div className='text-4xl text-center '>Loading...</div>;
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-4xl font-bold mb-4 text-center">User List</h1>
+      <table className="min-w-full bg-white border border-gray-200">
+        <thead className='bg-gradient-to-r from-sky-500 to-indigo-500'>
+          <tr>
+            <th className="py-2 px-4 border-b">ID</th>
+            <th className="py-2 px-4 border-b">Name</th>
+            <th className="py-2 px-4 border-b">Email</th>
+            <th className="py-2 px-4 border-b">Username</th>
+            <th className="py-2 px-4 border-b">Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id} className="hover:bg-gradient-to-r from-sky-200 to-indigo-200">
+              <td className="py-2 px-4 border-b">{user.id}</td>
+              <td className="py-2 px-4 border-b">{user.name}</td>
+              <td className="py-2 px-4 border-b">{user.email}</td>
+              <td className="py-2 px-4 border-b">{user.username}</td>
+              <td className="py-2 px-4 border-b">{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserList;
